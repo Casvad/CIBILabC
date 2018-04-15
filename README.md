d9b24683166c422ef9e5270292cf12546412b018
Carlos S <carlos.sanchez-v@mail.escuelaing.edu.co>
2018-04-15 15:06:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+sudo apt-get install libsdl2-dev
+g++ test.c -O test -lSDL2main -lSDL2
+./test
---
+++ test.c
@@ -0,0 +1,18 @@
+
+#include "SDL2/SDL.h"
+#include <stdio.h>
+
+
+
+int main()
+{
+	/*printf("Hello! This is my first C program with Ubuntu 11.10\n");*/
+	/* Do something more if you want */
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+                         "CIBI laboratory",
+                         "Is this a sandbox???",
+                         NULL);
+	printf ("Ur computer has been infected... sorry :) \n ");
+
+	return 0;
+}
